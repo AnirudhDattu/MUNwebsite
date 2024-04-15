c1ca8165ad03e2efef54657fbf48096785d096a7
Akaash Gupta <akaashgupta2005@gmail.com>
2024-04-15 20:57:04 +0530

    about1 responsive and about2 information changed

--- src/components/about2.jsx
+++ src/components/about2.jsx
@@ -13,19 +13,9 @@ const About2 = () => {
           <div className="text-white font-semibold text-start mt-9 ml-10 text-3xl tracking-wide lg:basis-1/4 basis-1/5">
             SIERRA
           </div>
-          <div className="text-white font-medium text-left md:text-justify text-lg md:mx-10 mx-6">
+          <div className="pb-6  text-white font-medium text-left md:text-justify text-lg md:mx-10 mx-6">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              delectus ad vero autem harum impedit cupiditate labore beatae! Eum
-              placeat quos pariatur. Velit reiciendis error aperiam provident
-              ut, cumque doloremque! Dolorum, eligendi? Reiciendis modi
-              eligendi, aperiam, nam totam nulla itaque esse similique,
-              reprehenderit facilis soluta accusamus quisquam necessitatibus
-              dolores expedita cum deleniti? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quisquam delectus ad vero autem
-              harum impedit cupiditate labore beatae! Eum placeat quos pariatur.
-              Velit reiciendis error aperiam provident ut, cumque doloremque!
-              Dolorum, eligendi? Reiciendis modi eligendi, aperiam, nam totam
+              Sierra Club at BML Munjal University is at the forefront of organizing impactful events, including Model United Nations (MUN). With a commitment to fostering a vibrant student community, Sierra has consistently spearheaded MUN conferences, offering platforms for around 100 participants to engage in stimulating debates and diplomacy simulations. The club's dedication to excellence is evident through its meticulous planning and execution, ensuring that each MUN event is a resounding success. Sierra Club goes beyond merely organizing conferences; it cultivates an environment where students hone their negotiation skills, expand their worldview, and forge lasting connections. As a pivotal player in the university's extracurricular landscape, Sierra continues to innovate and expand its MUN initiatives, promising exciting opportunities for students to develop leadership abilities and global awareness.
             </p>
           </div>
         </div>
@@ -33,19 +23,9 @@ const About2 = () => {
           <div className="text-white font-semibold text-start mt-9 ml-10 text-3xl tracking-wide lg:basis-1/4 basis-1/5">
             PAC
           </div>
-          <div className="text-white font-medium text-left md:text-justify text-lg md:mx-10 mx-6">
+          <div className="pb-6 text-white font-medium text-left md:text-justify text-lg md:mx-10 mx-6">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              delectus ad vero autem harum impedit cupiditate labore beatae! Eum
-              placeat quos pariatur. Velit reiciendis error aperiam provident
-              ut, cumque doloremque! Dolorum, eligendi? Reiciendis modi
-              eligendi, aperiam, nam totam nulla itaque esse similique,
-              reprehenderit facilis soluta accusamus quisquam necessitatibus
-              dolores expedita cum deleniti? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quisquam delectus ad vero autem
-              harum impedit cupiditate labore beatae! Eum placeat quos pariatur.
-              Velit reiciendis error aperiam provident ut, cumque doloremque!
-              Dolorum, eligendi? Reiciendis modi eligendi, aperiam, nam totam
+              The BML Munjal University Photography Club (PAC Club) fosters a vibrant community for aspiring photographers. PAC as a club bridge the gap between passion and purpose by offering a comprehensive program. Our club is like a fun adventure where folks can learn all about taking great photos and making awesome films. Members gain invaluable insights from professionals through lectures and workshops, refining both technical and creative skills. Group critiques provide constructive feedback, while photo walks hone visual storytelling through capturing the essence of local hotspots. PAC Club also recognizes the importance of equipment in skill development and strives to provide access to resources, fostering further exploration to the folks. 
             </p>
           </div>
         </div>
@@ -53,19 +33,9 @@ const About2 = () => {
           <div className="text-white font-semibold text-start mt-9 ml-10 text-3xl tracking-wide lg:basis-1/4 basis-1/5">
             LiQuiD
           </div>
-          <div className="text-white font-medium text-left md:text-justify text-lg  md:mx-10 mx-6">
+          <div className=" pb-6 text-white font-medium text-left md:text-justify text-lg  md:mx-10 mx-6">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              delectus ad vero autem harum impedit cupiditate labore beatae! Eum
-              placeat quos pariatur. Velit reiciendis error aperiam provident
-              ut, cumque doloremque! Dolorum, eligendi? Reiciendis modi
-              eligendi, aperiam, nam totam nulla itaque esse similique,
-              reprehenderit facilis soluta accusamus quisquam necessitatibus
-              dolores expedita cum deleniti? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quisquam delectus ad vero autem
-              harum impedit cupiditate labore beatae! Eum placeat quos pariatur.
-              Velit reiciendis error aperiam provident ut, cumque doloremque!
-              Dolorum, eligendi? Reiciendis modi eligendi, aperiam, nam totam
+              LiQuiD, the Literary, Quizzing and Debating Society of BML Munjal University, stands as a vibrant hub where passionate minds converge to exchange ideas and hone their skills. Through engaging events like "Chai pe Charcha," Mock Parliamentary Debates, and the renowned InQUIZitive competition, LiQuiD fosters a culture of awareness, articulate expression, and intellectual growth among students.  LiQuiD thrives on the enthusiastic participation of its community. Literary endeavors showcase captivating works, while quizzing sessions pulse with excitement and knowledge-sharing. Debating events spark thunderous discourse, leaving audiences awestruck by the depth of research and eloquence displayed.
             </p>
           </div>
         </div>
